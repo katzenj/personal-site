2185555efbee7b06ea44b55c097bad1886d2a8da
Jordan Katzen <jkatzen8@gmail.com>
2019-06-17 20:53:24 -0400

    moving to gatsby

---
+++ src/components/resumeSection.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import resumeStyles from '../styles/resumeSection.module.scss'
+
+const ResumeSection = (props) => {
+    var idx = 1
+    const bullets = props.bullets.map(bullet => <li key={idx++}>{bullet}</li>)
+
+    return (
+        <div className={resumeStyles.section}>
+            <div className={resumeStyles.columnMedium}>
+                <h3>{props.title}</h3>
+                <p>{props.company} - <em>{props.location}</em></p>
+                <p>{props.dates}</p>
+            </div>
+            <div className={resumeStyles.columnLarge}>
+                <ul>
+                    {bullets}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default ResumeSection
---
+++ src/posts/react.md
@@ -0,0 +1,11 @@
+---
+title: "Second Post"
+date: "2019-05-18"
+author: "Jordan Katzen"
+---
+
+Other post
+
+## Test Title 2
+
+1. Woohoo
